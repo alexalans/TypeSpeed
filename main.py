3f99b699105da13dcee636fa290bb1e527c780c1
Alexa Lans <alexa@extralexa.nl>
2024-08-19 11:06:10 +0300

    Add restart button functionality

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import random
 
 language = "English"
 words_sent = 0
+word_objects_list = None
+countdown = None
 
 
 class Word:
@@ -12,12 +14,44 @@ class Word:
         self.text = a_word
         self.color = "black"
 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def show_result():
+    pass
+
+
+def timer(seconds):
+    time.config(text=f"Time left: {seconds}")
+    global countdown
+    countdown = window.after(1000, timer, seconds - 1)
+    # if count == 0:
+    #     show_result()
+
+
 def restart_test():
-    pass
+    word_entry.delete(0, 'end')
+    type_this.config(text= "Type this:")
+    window.after_cancel(countdown)
+    window.focus_set()
+    global words_sent
+    words_sent = 0
+    word_entry.insert(0, "Click here to start!")
+    word_entry.bind("<Button-1>", start_test)
+    time.config(text=f"Time left:")
+
+    # start_test(event=0)
 
 
-def change_to_cyrillic():
-    pass
+def switch_language():
+    global language
+    if language == "English":
+        language = "Bulgarian"
+        change_language.config(text="English")
+    else:
+        language = "English"
+        change_language.config(text="Български")
+
 
 
 def submit_word(event):
@@ -34,10 +68,15 @@ def submit_word(event):
     word_entry.delete(0, 'end')
     update_text()
 
+
 def start_test(event):
+    word_entry.unbind("<Button 1>")
+    global word_objects_list
+    word_objects_list = load_text()
     update_text()
     word_entry.delete(0, 'end')
-    # TODO STARTING THE TIMER ETC
+    type_this.config(text= "Type this:")
+    timer(60)
 
 
 def update_text():
@@ -58,20 +97,24 @@ canvas.config(width=200, height=70)
 canvas.create_image(100, 35, image=logo)
 canvas.grid(column=1, row=1, columnspan=3)
 
-type_this = Label(text="Type this:", justify="left", anchor="w", font=("Arial", 13))
+type_this = Label(text="", justify="left", anchor="w", font=("Arial", 13))
 type_this.grid(column=1, row=2, sticky=W + E, pady=5)
 
-# ----- Text loading --- #
-
-with open("text.json", mode="r", encoding='utf-8') as texts:
-    content = json.load(texts)
-
-random_nr = random.randint(1, 3)
-
-word_objects_list = [Word(word) for word in content[language][f"{random_nr}"].split()]
+def load_text():
+    with open("text.json", mode="r", encoding='utf-8') as texts:
+        content = json.load(texts)
+    random_nr = random.randint(1, 3)
+    word_objects_list = [Word(word) for word in content[language][f"{random_nr}"].split()]
+    return word_objects_list
 
 text_to_type = Text(height=10, wrap="word")
 text_to_type.config(state=NORMAL)
+text_to_type.insert("end", "Welcome to TypeSpeed! \n\n"
+                           "This program will measure your typing speed in words per minute. \n"
+                           "You have 60 seconds to type as much of the test text as you can. \n"
+                           "Every time you hit spacebar, your word will be submitted and evaluated. \n"
+                           "The test text will appear here as soon as you click into the typing box. \n\n"
+                           "Good luck! ")
 text_to_type.grid(column=1, row=3, columnspan=3, sticky=W + E)
 
 text_to_type.tag_configure(tagName="green", foreground="green")
@@ -90,7 +133,10 @@ word_entry.grid(column=1, row=6, columnspan=3, pady=5, sticky=W + E)
 restart = Button(text="Restart", command=restart_test)
 restart.grid(column=1, row=7, padx=3)
 
-cyrillic = Button(text="Кирилица (Български)", width=30, command=change_to_cyrillic, pady=5)
-cyrillic.grid(column=2, row=7, columnspan=2, pady=3)
+change_language = Button(text="Български", width=30, command=switch_language, pady=5)
+change_language.grid(column=2, row=7, columnspan=1, pady=3, sticky=W + E)
+
+time = Label(text="Time left:", justify="left", anchor="w", font=("Arial", 13))
+time.grid(column=3, row=7, columnspan=1, padx=25, pady=10, sticky=W + E)
 
 window.mainloop()
